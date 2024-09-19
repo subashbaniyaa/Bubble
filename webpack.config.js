1076935add6479f745200c7bc3867bd1a358da67
Subash Baniya <154237809+sarkardocs@users.noreply.github.com>
2024-09-19 20:39:47 +0545

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,55 @@
-const path = require('path');
+/* Configure HTMLWebpack plugin */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+/* Configure BrowserSync */
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const BrowserSyncPluginConfig = new BrowserSyncPlugin({
+    host: 'localhost',
+    port: 3000,
+}, config = {
+    reload: false
+});
 
 module.exports = {
-  mode: 'production',
-  entry: './src/main.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.built.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(glsl|vs|fs|vert|frag|worker)$/,
-        use: 'raw-loader',
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
+    mode: "production",
+    // devtool: "inline-source-map",
+    entry: "./src/main.ts",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "main.built.js"
+    },
+    node: { fs: 'empty' },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                exclude: /[\/\\](node_modules|bower_components|public)[\/\\]/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(glsl|vs|fs|vert|frag|worker)$/,
+                exclude: /node_modules/,
+                use: 'raw-loader'
+            }
+        ]
+    },
+    plugins: [HTMLWebpackPluginConfig, BrowserSyncPluginConfig]
 };
