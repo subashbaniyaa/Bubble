1a3dd2fbc8da7185f583386c743e6a4bf614b2e6
Subash Baniya <154237809+sarkardocs@users.noreply.github.com>
2024-09-19 19:51:28 +0545

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,19 @@
 const path = require('path');
-
-/* Configure HTMLWebpack plugin */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: __dirname + '/src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-});
-
-/* Configure BrowserSync */
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const BrowserSyncPluginConfig = new BrowserSyncPlugin({
-    host: 'localhost',
-    port: 3000,
-}, config = {
-    reload: false
-});
 
 module.exports = {
     mode: "development",
-    // devtool: "inline-source-map",
     entry: "./src/main.ts",
     output: {
-        path: path.resolve(__dirname, 'dist'),  // <-- Change the output directory to 'dist'
+        path: path.resolve(__dirname, 'dist'), // Output to 'dist' directory
         filename: "main.built.js"
     },
-    node: { fs: 'empty' },
     resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: [".ts", ".tsx", ".js"]
     },
     module: {
         rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
             {
                 test: /\.tsx?$/,
                 loader: 'ts-loader',
@@ -40,11 +21,8 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                exclude: /[\/\\](node_modules|bower_components|public)[\/\\]/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.(glsl|vs|fs|vert|frag|worker)$/,
@@ -53,5 +31,16 @@ module.exports = {
             }
         ]
     },
-    plugins: [HTMLWebpackPluginConfig, BrowserSyncPluginConfig]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            reload: false
+        })
+    ]
 };
